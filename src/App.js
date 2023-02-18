4552d91ae59757f43163905f27061bf698dd2bec
INS140 <IsaacNSaettel@gmail.com>
2023-02-18 06:59:40 -0800

    styles and app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import getAdvice from "./getAdvice";
 
 function App() {
   const [advice, setAdvice] = useState({})
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  window.onresize = () => setWindowWidth(window.innerWidth)
 
   const changeAdvice = async () => {
     let slip = await getAdvice()
@@ -13,14 +16,16 @@ function App() {
     changeAdvice()
   }, [])
 
-  useEffect(() => {
-    console.log(advice)
-  }, [advice])
-
   return (
     <div className="container">
-      <h1>Advice #{advice.id}</h1>
-      <p>{advice.advice}</p>
+      <h1 className="advice-id">ADVICE #{advice.id}</h1>
+      <p className="advice-text">"{advice.advice}"</p>
+      {
+        windowWidth > 375
+          ? <img src="./images/pattern-divider-desktop.svg" alt="divider" className="divider" />
+          : <img src="./images/pattern-divider-mobile.svg" alt="divider" className="divider" />
+      }
+      <button className="roll-advice" onClick={changeAdvice}><img src="./images/icon-dice.svg" alt="dice icon" /></button>
     </div>
   );
 }
